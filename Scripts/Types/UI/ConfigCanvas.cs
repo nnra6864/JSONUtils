380a4be323cfa29c98dbe174089af85c902fcbf0
nnra6864 <enenragd@gmail.com>
2025-01-10 23:16:11 +0100

    Added a very basic implementation of the Canvas and CanvasScaler

---
+++ Scripts/Types/UI/ConfigCanvas.cs
@@ -0,0 +1,77 @@
+using System;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using UnityEngine;
+
+namespace UnityJSONUtils.Scripts.Types.UI
+{
+    [Serializable]
+    public class ConfigCanvas
+    {
+        [JsonConverter(typeof(StringEnumConverter))]
+        [JsonProperty] public RenderMode RenderMode;
+        [JsonProperty] public bool PixelPerfect;
+        [JsonProperty] public int SortOrder;
+        [JsonProperty] public int TargetDisplay;
+        [JsonProperty] public string CameraName;
+
+        public ConfigCanvas()
+        {
+            RenderMode = RenderMode.ScreenSpaceOverlay;
+            PixelPerfect = false;
+            SortOrder = 0;
+            TargetDisplay = 0;
+            CameraName = "";
+        }
+
+        public ConfigCanvas(Canvas canvas)
+        {
+            RenderMode    = canvas.renderMode;
+            PixelPerfect  = canvas.pixelPerfect;
+            SortOrder     = canvas.sortingOrder;
+            TargetDisplay = canvas.targetDisplay;
+            CameraName    = canvas.worldCamera.gameObject.name;
+        }
+
+        public ConfigCanvas(
+            RenderMode mode = RenderMode.ScreenSpaceOverlay,
+            bool pixelPerfect  = false,
+            int sortOrder = 0,
+            int targetDisplay = 0,
+            string cameraName = ""
+            )
+        {
+            RenderMode = mode;
+            PixelPerfect = pixelPerfect;
+            SortOrder = sortOrder;
+            TargetDisplay = targetDisplay;
+            CameraName = cameraName;
+        }
+        
+        public static implicit operator ConfigCanvas(Canvas canvas) => new(canvas);
+        
+        public static implicit operator Canvas(ConfigCanvas configCanvas) => new()
+        {
+            renderMode = configCanvas.RenderMode,
+            pixelPerfect = configCanvas.PixelPerfect,
+            sortingOrder = configCanvas.SortOrder,
+            targetDisplay = configCanvas.TargetDisplay,
+            worldCamera = GameObject.Find(configCanvas.CameraName)?.GetComponent<Camera>()
+        };
+
+        /// <summary>
+        /// Updates canvas values with ConfigCanvas values
+        /// </summary>
+        /// <param name="canvas">Canvas to update</param>
+        /// <returns></returns>
+        public Canvas UpdateCanvas(Canvas canvas)
+        {
+            canvas.renderMode = RenderMode;
+            canvas.pixelPerfect = PixelPerfect;
+            canvas.sortingOrder = SortOrder;
+            canvas.targetDisplay = TargetDisplay;
+            canvas.worldCamera = GameObject.Find(CameraName)?.GetComponent<Camera>();
+            return canvas;
+        }
+    }
+}
---
+++ Scripts/Types/UI/ConfigCanvasScaler.cs
@@ -0,0 +1,133 @@
+using System;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace UnityJSONUtils.Scripts.Types.UI
+{
+    [Serializable]
+    public class ConfigCanvasScaler
+    {
+        [Header("General")]
+        [JsonConverter(typeof(StringEnumConverter))]
+        [JsonProperty] public CanvasScaler.ScaleMode ScaleMode;
+        [JsonProperty] public float ReferencePixelsPerUnit;
+        
+        [Header("Constant Pixel Size")]
+        [JsonProperty] public float ScaleFactor;
+        
+        [Header("Scale With Screen Size")]
+        [JsonProperty] public ConfigVector2 ReferenceResolution;
+        [JsonConverter(typeof(StringEnumConverter))]
+        [JsonProperty] public CanvasScaler.ScreenMatchMode ScreenMatchMode;
+        [Range(0, 1)] public float MatchWidthOrHeight;
+
+        [Header("Constant Physical Size")]
+        [JsonConverter(typeof(StringEnumConverter))]
+        [JsonProperty] public CanvasScaler.Unit PhysicalUnit;
+        [JsonProperty] public float FallbackScreenDPI;
+        [JsonProperty] public float DefaultSpriteDPI;
+
+        public ConfigCanvasScaler()
+        {
+            // General
+            ScaleMode              = CanvasScaler.ScaleMode.ConstantPixelSize;
+            ReferencePixelsPerUnit = 1;
+            
+            // Constant Pixel Size
+            ScaleFactor         = 1;
+            
+            // Scale With Screen Size
+            ReferenceResolution = new(800, 600);
+            ScreenMatchMode     = CanvasScaler.ScreenMatchMode.MatchWidthOrHeight;
+            MatchWidthOrHeight  = 0;
+            
+            // Constant Physical Size
+            PhysicalUnit        = CanvasScaler.Unit.Points;
+            FallbackScreenDPI   = 96;
+            DefaultSpriteDPI    = 96;
+        }
+
+        public ConfigCanvasScaler(CanvasScaler scaler)
+        {
+            // General
+            ScaleMode              = scaler.uiScaleMode;
+            ReferencePixelsPerUnit = scaler.referencePixelsPerUnit;
+
+            // Constant Pixel Size
+            ScaleFactor = scaler.scaleFactor;
+
+            // Scale With Screen Size
+            ReferenceResolution = scaler.referenceResolution;
+            ScreenMatchMode     = scaler.screenMatchMode;
+            MatchWidthOrHeight  = scaler.matchWidthOrHeight;
+
+            // Constant Physical Size
+            PhysicalUnit      = scaler.physicalUnit;
+            FallbackScreenDPI = scaler.fallbackScreenDPI;
+            DefaultSpriteDPI  = scaler.defaultSpriteDPI;
+        }
+
+        public ConfigCanvasScaler(
+            // General
+            CanvasScaler.ScaleMode scaleMode,
+            float referencePixelsPerUnit,
+            
+            // Constant Pixel Size
+            float scaleFactor,
+            
+            // Scale With Screen Size
+            Vector2 referenceResolution,
+            CanvasScaler.ScreenMatchMode screenMatchMode,
+            float matchWidthOrHeight,
+            
+            // Constant Physical Size
+            CanvasScaler.Unit physicalUnit,
+            float fallbackScreenDPI,
+            float defaultSpriteDPI
+        )
+        {
+            // General
+            ScaleMode              = scaleMode;
+            ReferencePixelsPerUnit = referencePixelsPerUnit;
+
+            // Constant Pixel Size
+            ScaleFactor = scaleFactor;
+
+            // Scale With Screen Size
+            ReferenceResolution = referenceResolution;
+            ScreenMatchMode     = screenMatchMode;
+            MatchWidthOrHeight  = matchWidthOrHeight;
+
+            // Constant Physical Size
+            PhysicalUnit      = physicalUnit;
+            FallbackScreenDPI = fallbackScreenDPI;
+            DefaultSpriteDPI  = defaultSpriteDPI;
+        }
+
+        public static implicit operator ConfigCanvasScaler(CanvasScaler scaler) => new(scaler);
+
+        public CanvasScaler UpdateCanvasScaler(CanvasScaler scaler)
+        {
+            // General
+            scaler.uiScaleMode = ScaleMode;
+            scaler.referencePixelsPerUnit = ReferencePixelsPerUnit;
+            
+            // Constant Pixel Size
+            scaler.scaleFactor = ScaleFactor;
+            
+            // Scale With Screen Size
+            scaler.referenceResolution = ReferenceResolution;
+            scaler.screenMatchMode = ScreenMatchMode;
+            scaler.matchWidthOrHeight = MatchWidthOrHeight;
+            
+            // Constant Physical Size
+            scaler.physicalUnit = PhysicalUnit;
+            scaler.fallbackScreenDPI = FallbackScreenDPI;
+            scaler.defaultSpriteDPI = DefaultSpriteDPI;
+            
+            return scaler;
+        }
+    }
+}
